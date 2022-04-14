9b85336f994061df7f6902d9819b50ac6c54f306
Paulo Victor <paulovmel@gmail.com>
2022-04-13 22:58:05 -0300

    feat: creates PersonAgent
    Co-authored-by: Lucas Gabriel <lucasbsb100@gmail.com>

--- person_model.py
+++ person_model.py
@@ -1,3 +1,13 @@
+from mesa import Agent, Model
+from mesa.time import RandomActivation
+
+class PersonAgent(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.isContaminated = False
+        self.encountersWithContaminated = 0      
+
+
 class PersonModel(Model):
     def __init__(self, N):
         self.number_of_agents = N
