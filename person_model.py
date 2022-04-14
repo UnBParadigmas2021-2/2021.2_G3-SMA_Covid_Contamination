3c58fb96ca111b026f4154cf1764daf633bff608
Paulo Victor <paulovmel@gmail.com>
2022-04-13 23:00:44 -0300

    feat: adds contamination logic in PersonAgent
    Co-authored-by: Lucas Gabriel <lucasbsb100@gmail.com>

--- person_model.py
+++ person_model.py
@@ -5,11 +5,25 @@ class PersonAgent(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.isContaminated = False
-        self.encountersWithContaminated = 0      
+        self.encountersWithContaminated = 0
 
     def step(self):
         contamination_level = "contaminated" if self.isContaminated else "not contaminated"
         print("Hi, I am agent " + str(self.unique_id) + " and I'm " + contamination_level)
+
+        other_agent = self.random.choice(self.model.schedule.agents)
+        # print("Hi, I am agent " + str(self.unique_id) + " and I've encountered with agent: " + str(other_agent.unique_id))
+
+
+        if self.isContaminated == True:
+          other_agent.encountersWithContaminated += 1
+        else:
+          if other_agent.isContaminated == True:
+            self.encountersWithContaminated += 1
+
+        if self.encountersWithContaminated > 3:
+          self.isContaminated = True        
+
 
 class PersonModel(Model):
     def __init__(self, N):
