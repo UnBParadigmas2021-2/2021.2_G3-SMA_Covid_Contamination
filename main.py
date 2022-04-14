f406f00783051a93bcc199210c15f0db4076582b
Paulo Victor <paulovmel@gmail.com>
2022-04-13 22:54:49 -0300

    feat: creates main function and initialize the personModel

---
+++ main.py
@@ -0,0 +1,12 @@
+
+from person_model import PersonModel
+
+
+if __name__ == "__main__":
+    personModel = PersonModel(50)
+
+    for i in range(50):
+        personModel.step()
+        print("\n ---------------- Iteration " + str(i + 1) + " ---------------- \n")
+
+
