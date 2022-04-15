f6d16b1e2e513dad0b701628bf8895186ece02da
Lucas <lucasbsb100@gmail.com>
2022-04-15 12:01:41 -0300

    Reorganizando arquivos
    Co-authored-by: Kleidson Alves <kleidsonalves15@gmail.com>
    Co-authored-by: Lucas Rodrigues <lucasro229@gmail.com>

--- person_model.py
+++ person_model.py
@@ -1,66 +1,26 @@
-from mesa import Agent, Model
+from mesa import Model
 from mesa.time import RandomActivation
+from mesa.space import MultiGrid
+from person_agent import PersonAgent
 
-
-class PersonAgent(Agent):
-    recoveryDay = 7
-
-    def __init__(self, unique_id, model):
-        super().__init__(unique_id, model)
-        self.isContaminated = False
-        self.isSymptomatic = True
-        self.daysContaminated = 0
-        self.isTransmitter = False
-
-    def probabilityInfection(self, symptomatic):
-        if not symptomatic:
-            return self.random.choices([True, False], [0.58, 0.42])[0]
-        return True
-
-    def updateStatus(self):
-        if self.daysContaminated == self.recoveryDay:
-            self.isContaminated = False
-
-        if self.isContaminated:
-            self.daysContaminated += 1
-        else:
-            self.daysContaminated = 0
-
-    def verifyContact(self):
-        other_agent = self.random.choice(self.model.schedule.agents)
-
-        if self.isContaminated == True:
-            other_agent.isContaminated = self.probabilityInfection(
-                self.isSymptomatic)
-
-        elif other_agent.isContaminated == True and not self.isContaminated:
-            self.isContaminated = self.probabilityInfection(
-                other_agent.isSymptomatic)
-            self.isSymptomatic = self.random.choice([True, False])
-
-    def printStatus(self):
-        contamination_level = "contaminated" if self.isContaminated else "not contaminated"
-        print("Hi, I am agent " + str(self.unique_id) +
-              " and I'm " + contamination_level)
-
-    def step(self):
-
-        self.verifyContact()
-        self.updateStatus()
-        self.printStatus()
-
-
+width = 20
+height = 20
 class PersonModel(Model):
-    def __init__(self, N):
-        self.number_of_agents = N
-        self.schedule = RandomActivation(self)
+  def __init__(self, N):
+      self.numAgents = N
+      self.schedule = RandomActivation(self)
+      self.grid = MultiGrid(width,height, True)
+      self.running = True
 
-        for i in range(self.number_of_agents):
-            agent = PersonAgent(i, self)
-            self.schedule.add(agent)
+      for i in range(self.numAgents):
+          x = self.random.randrange(width)
+          y = self.random.randrange(height)
+          agent = PersonAgent(i, self)
+          self.schedule.add(agent)
+          self.grid.place_agent(agent, (x, y))
+          if i == 0:
+              agent.isContaminated = True
+              agent.isTransmitter = True
 
-            if i == 0:
-                agent.isContaminated = True
-
-    def step(self):
-        self.schedule.step()
+  def step(self):
+      self.schedule.step()
