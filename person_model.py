50e874b8ffd18c767b61eb004ae4e971224000b6
Pedrok99 <pedrohlima20@gmail.com>
2022-04-16 14:35:56 -0300

    fix agent collision
    Co-authored-by: EduardoPicolo <eduardonpicolo@gmail.com>
    Co-authored-by: Matheus73 <matheuscoding@gmail.com>
    Co-authored-by: Anacarolcs <ancarolcs@gmail.com>

--- person_model.py
+++ person_model.py
@@ -2,19 +2,29 @@ from mesa import Model
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
 from person_agent import PersonAgent
+from random import randrange
 
 width = 20
 height = 20
+
+def genPos():
+    x = randrange(width)
+    y = randrange(height)
+    return x, y
+
 class PersonModel(Model):
   def __init__(self, N):
       self.numAgents = N
       self.schedule = RandomActivation(self)
       self.grid = MultiGrid(width,height, True)
       self.running = True
+      self.filledPositions = []
 
       for i in range(self.numAgents):
-          x = self.random.randrange(width)
-          y = self.random.randrange(height)
+          x, y = genPos()
+          while (x,y) in self.filledPositions:
+            x, y = genPos()
+          self.filledPositions.append((x,y))
           agent = PersonAgent(i, self)
           self.schedule.add(agent)
           self.grid.place_agent(agent, (x, y))
@@ -24,3 +34,4 @@ class PersonModel(Model):
 
   def step(self):
       self.schedule.step()
+
